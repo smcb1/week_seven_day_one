1f20b2eaf74ab9e92f2bb7fc5a7af04f35ba64c5
Simon McBride <rexnash@gmail.com>
2019-02-18 18:00:21 +0000

    Add homework files and folders

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      message: "Enter some text below to count the words.",
+      enteredWords: ""
+    },
+    methods: {
+      countWords: function(){
+
+      }
+
+    }
+  });
+});
